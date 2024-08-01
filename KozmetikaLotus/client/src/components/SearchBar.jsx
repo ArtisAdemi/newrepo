4b871c9ca361d6e4cbf0d0e91c35c1dbde96dcaa
ArtisAdemi <artis.ademi@gmail.com>
2024-08-01 23:35:00 +0200

    redesigned search bar

--- KozmetikaLotus/client/src/components/SearchBar.jsx
+++ KozmetikaLotus/client/src/components/SearchBar.jsx
@@ -59,26 +59,27 @@ const SearchBar = () => {
         <div className='relative flex items-center md:p-2 md:pr-4 md:-ml-6 md:end-1 md:mb-[3px] cursor-pointer'>
           <input value={productName} className='border border-1 w-[100px] md:w-[200px] lg:w-[250px] border-[#0C0C0C4F] rounded-md p-1 px-6 ' style={{color: 'black'}} type="text" onChange={handleInputChange}  placeholder='Kerko...'/>
           {searchModal && 
-            <div className='modal rounded-2xl absolute top-20 left-50 right-50 bg-[#FAF9F5] w-[300px] px-4' // Close modal when not hovering
+            <div className='modal rounded-2xl absolute top-20 left-50 right-50 bg-[#FAF9F5] shadow-lg w-[300px] px-4' // Close modal when not hovering
               style={{ top: '100%', left: '50%', transform: 'translateX(-50%)' }} // Center modal directly below the Profile Icon
             >
               <div className='w-full flex justify-center'>
                 <div className='w-[90%] justify-center'>
                   <div className='test flex justify-center flex-col items-center'>
                     <div className='mt-4 text-start items-start align-middle w-full pb-4'>
-                      <div className='flex flex-col'>
+                      <div className='flex flex-col border-[0.5px] border-x-0 border-t-0 border-gray-500'>
+                          <h2 className='text-gray-500 font-semibold'>Rezultatet:</h2>
                         {/* Product cart */}
                         {products.map((product, index) => (
-                          <div key={index} className='border-[0.1px] py-3 m-2 rounded-md border-gray-200' onClick={() => onClickProduct(product)}>
+                          <div key={index} className='border-[0.5px] border-x-0 border-b-0 py-1 m-1 border-gray-500' onClick={() => onClickProduct(product)}>
                           {/* <hr className='border-0 h-[0.3px] bg-black my-4'/> */}
-                            <div className='flex w-full'>
+                            <div className='flex w-full max-h-[40px]'>
                               <div className='w-1/3'>
                                 {product.Images && product.Images.length > 0 ? (
                                   <img src={`/uploads/${product.Images[0].fileName}`}
-                                    className='object-contain h-24 w-24'
+                                    className='object-contain h-12 w-full'
                                     alt="" />
                                 ) : (
-                                  <div className='object-contain h-24 w-24 bg-gray-200 flex items-center justify-center'>
+                                  <div className='object-contain h-12 w-full bg-gray-200 flex items-center justify-center'>
                                     No Image
                                   </div>
                                 )}
