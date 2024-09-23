8407da0c3b8a2eda9588dbf452061785b0017e67
ArtisAdemi <artis.ademi@gmail.com>
2024-09-23 02:12:51 +0200

    temp backend url

--- KozmetikaLotus/client/src/services/backendUrl.js
+++ KozmetikaLotus/client/src/services/backendUrl.js
@@ -1,4 +1,5 @@
-const API_URL = "https://backend-koz-lotus.duckdns.org/api";
+// const API_URL = "https://backend-koz-lotus.duckdns.org/api";
+const API_URL = "http://109.199.98.248:3001/api";
 // const API_URL = "https://backend.kozmetikalotus.com/api";
 // const API_URL = "http://localhost:3001/api";
 
